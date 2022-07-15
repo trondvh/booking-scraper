6a2f7e4753da809b2a3df2fdddb1e6b0aff2e30b
Trond Viggo Håpnes <trond.hapnes@gmail.com>
2022-07-15 13:50:09 +0200

    New r360owner page / layout

--- src/main.py
+++ src/main.py
@@ -15,20 +15,19 @@ def login():
     }
 
     s = requests.session()
-    s.post('https://hafjellkvitfjell.restech.se/r360/owner/login.php?site=hafjellkvitfjell&lang=NO', data=login_payload, timeout=15)
+    s.post('https://hafjellkvitfjell.r360owner.se/r360/owner/login.php?lang=no', data=login_payload, timeout=15)
     
     return(s)
 
 def fetch_reservations(session):
-    reservations_url = "https://hafjellkvitfjell.restech.se/r360/owner/reservations.php"
+    reservations_url = "https://hafjellkvitfjell.r360owner.se/r360/owner/reservations.php"
     return(session.get(reservations_url))
 
 def main():
     session = login()
 
     soup = bs(fetch_reservations(session).text, 'html.parser')
-    table = soup.find("table", {"class": "reservationsTable"})
-    
+    table = soup.find("table", {"class": "reservations-table r360-table"})
     client = MongoClient(environ["DBSTRING"])
 
     db = client.booking_scraper
@@ -36,28 +35,36 @@ def main():
 
     for row in table.findAll("tr"):
         cells = row.findAll("td")
-        if len(cells) == 10:
-            parse_guests = re.findall(r'\d+', str(cells[3]))
-            parse_dates = re.findall(
-                r'\d\d\d\d-\d\d-\d\d', str(cells[2].find(text=True)))
+        booking_id = cells[1].find(text=True)
 
-            guests = list(map(int, parse_guests))
+        if booking_id:
+            # parse_guests = re.findall(r'\d+', str(cells[3]))
+            # guests = list(map(int, parse_guests))
+            
+            parse_dates = re.findall(
+                r'\d\d-\d\d-\d\d', str(cells[2].find(text=True)))
 
             reservation = {}
-            booking_id = cells[1].find(text=True)
-            reservation['adults'] = guests[0]
-            reservation['children'] = guests[1]
+            # reservation['adults'] = guests[0]
+            # reservation['children'] = guests[1]
             reservation['from_date'] = datetime.strptime(
-                parse_dates[0], '%Y-%m-%d')
-            reservation['to_date'] = datetime.strptime(parse_dates[1], '%Y-%m-%d')
-            reservation['payout'] = float(
-                re.sub(r'[^0-9.]', '', cells[8].find(text=True)))
+                parse_dates[0], '%d-%m-%y')
+            reservation['to_date'] = datetime.strptime(
+                parse_dates[1], '%d-%m-%y')
+ 
+            num_replacements = [(" ", ""), (",", ".")]
+            payout = cells[7].find(text=True)
+
+            for pat, repl in num_replacements:
+                payout = re.sub(pat, repl, payout)
+
+            reservation['payout'] = float(payout)
+            
+            print(booking_id)
+            print(reservation)
 
             result = col.update_one({'_id': booking_id}, {
                                     "$set": reservation}, upsert=True)
-
-            print(cells[1].find(text=True), cells[2].find(
-                text=True), cells[3], cells[8].find(text=True))
 
 if __name__ == "__main__":
     required_env_vars = {"EMAIL", "PASSWORD", "DBSTRING"}
